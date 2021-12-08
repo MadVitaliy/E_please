3aa94287703e01eb9b50b7621e56823f2938f80a
Mad Vitaliy <omelchenko.vitaliy1@gmail.com>
2021-12-09 00:33:54 +0200

    add project base structure

---
+++ src/components/About/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import i from './About.module.css';
+
+console.log(i);
+
+const About = () => {
+    return (
+        <>
+            This project is been creating by Uulia Pisnur and Vitaliy Omelchenko for Evgen Batrak.
+            Wish us good lack!
+        </>
+    );
+};
+export default About;
---
+++ src/components/Settinngs/Settings.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import i from './Settings.module.css';
+
+console.log(i);
+
+const Settings = () => {
+    return (
+        <>
+            Settings page
+        </>
+    );
+};
+export default Settings;
