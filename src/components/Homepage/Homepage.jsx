f72d9a1d5fa3f2bdafbeda3df444dd537cac47f8
nzbdfyulya <83024463+nzbdfyulya@users.noreply.github.com>
2021-12-11 22:44:27 +0200

    create-toggle

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import i from './Homepage.module.css';
 import DeviceTab from "./DeviceTab/DeviceTab";
 
+import Heater from './DeviceTab/images/Heater.jpg';
+import Lamp from './DeviceTab/images/Lamp.jpg';
+import Backlight from './DeviceTab/images/Backlight.jpg';
+
+
 console.log(i);
 const CONTROL_TYPE = {
     SWITCHER: 0,
     SLIDER: 1,
     VALUE: 2
 };
+
 const CONNECTED = true;
 let devices =
     [
@@ -16,21 +22,24 @@ let devices =
             name: "Heater",
             protocol: "some iot protocol",
             status: CONNECTED,
-            control_type: CONTROL_TYPE.VALUE
+            control_type: CONTROL_TYPE.VALUE,
+            img: Heater,
         },
         {
             id: 2,
             name: "Lamp",
             protocol: "some iot protocol",
             status: CONNECTED,
-            control_type: CONTROL_TYPE.SWITCHER
+            control_type: CONTROL_TYPE.SWITCHER,
+            img: Lamp,
         },
         {
             id: 3,
             name: "Backlight",
             protocol: "some iot protocol",
             status: !CONNECTED,
-            control_type: CONTROL_TYPE.SLIDER
+            control_type: CONTROL_TYPE.SLIDER,
+            img: Backlight,
         }
     ];
 
