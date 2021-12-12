c7a217c6588acc12949d3f12a550cac5d51f0a84
Mad Vitaliy <omelchenko.vitaliy1@gmail.com>
2021-12-13 00:55:16 +0200

    added redux. added bootstrup. added new/edit device component. added presentetion and conteiner components. start to implement logic for devices

--- src/components/AddEditDevice/AddEditDeviceContainer.jsx
+++ src/components/AddEditDevice/AddEditDeviceContainer.jsx
@@ -3,46 +3,9 @@ import React from 'react';
 import {useSelector} from "react-redux";
 import AddEditDevice from "./AddEditDevice";
 
-// <div className={i.page}>
-//     <FormText>TODO: add something to choose device icon</FormText>
-//
-//     <div>
-//         <FormLabel>Device name:</FormLabel>
-//         <FormControl placeholder="Enter device name"/>
-//         <FormLabel className="text-muted">'Speakers' for example</FormLabel>
-//     </div>
-//
-//     <div>
-//         <FormLabel>Protocol:</FormLabel>
-//         <FormSelect>
-//             <option value="1">Protocol #1</option>
-//             <option value="2">Protocol #2</option>
-//             <option value="3">Protocol #2</option>
-//         </FormSelect>
-//         <FormText className="text-muted">TODO: get the list of available protocols thought props and create options with map</FormText>
-//     </div>
-//
-//     <div>
-//         <FormLabel>Control type:</FormLabel>
-//         <FormSelect>
-//             <option value="1">Toggle</option>
-//             <option value="2">Slider</option>
-//             <option value="3">Value</option>
-//         </FormSelect>
-//         <FormText className="text-muted">TODO: get the list of available protocols thought props and create options with map</FormText>
-//     </div>
-//
-//     <Button variant="primary" type="submit">
-//         Add/Save
-//     </Button>
-//     <div>
-//         <FormText className="text-muted">TODO: add logic to button</FormText>
-//     </div>
-// </div>
-
-
 const AddEditDeviceContainer = () => {
     let device_default_name = "Enter device name";
+    //TODO: get protocols list from store
     let protocols = ["Protocol #1", "Protocol #2","Protocol #3"];
 
     return (
